1523e756d834c7d6a63cd8d6769cc7bfd82148df
Yuragranovsky <yuragranovsky@gmail.com>
2021-12-08 16:24:42 +0200

    final commit

--- app.js
+++ app.js
@@ -26,16 +26,16 @@ array1.unshift(-100);
 //console.log(array1.join(':'))
 /***************************************************************** */
 //inserts at middle
-array1.splice(4,0,'Hello');
+array1.splice(4, 0, 'Hello');
 console.log(array1)
 
 //slice & splice
 //slice returns copy of a part of array, splice removes a part of array
 const array3 = array1.slice();
-console.log(array1.splice(4,3), 'splice result of array1');
+console.log(array1.splice(4, 3), 'splice result of array1');
 console.log(array1, 'array1 after splice')
 array1 = array3.slice();
-console.log(array1.slice(4,7), 'slice result of array1');
+console.log(array1.slice(4, 7), 'slice result of array1');
 console.log(array1, 'array1 after slice')
 //removes first element
 // console.log(array1.shift(), 'result of shift')
@@ -59,23 +59,23 @@ array1 = [1, 2, 3, 4, 5, 20];
 //reduce
 console.log(array1.reduce((r, e) => r * e))
 function maxMinAvg(array) {
-   
-        const res = array.reduce((r, e)=>{
-             r.max = Math.max(r.max, e);
-             r.min = Math.min(r.min, e);
-             r.total += e
-             return r
-         }, {min: array[0], max: array[0], total: 0})
-         return `maximal value - ${res.max}, minimal value - ${res.min}, average value - ${Math.round(res.total/array.length)}`  
-     
+
+    const res = array.reduce((r, e) => {
+        r.max = Math.max(r.max, e);
+        r.min = Math.min(r.min, e);
+        r.total += e
+        return r
+    }, { min: array[0], max: array[0], total: 0 })
+    return `maximal value - ${res.max}, minimal value - ${res.min}, average value - ${Math.round(res.total / array.length)}`
+
 }
 console.log(maxMinAvg(array1));
 //required output (average should be as an integer number)
 //Maximal value is 5, minimal value is 1, average value is 3 
 /******************************************************* */
-Array.prototype.map = function(mapper) {
+Array.prototype.map = function (mapper) {
     console.log(this);
-    
+
     const newArray = [];
     this.forEach((value, index, array) => {
         newArray[index] = mapper(value, index, array);
@@ -84,7 +84,7 @@ Array.prototype.map = function(mapper) {
     return newArray;
 }
 /************************************************************** */
-array1 = [1,2,3];
+array1 = [1, 2, 3];
 // const array4 = array1.map((e, i) => i + '. ' + e * 2);
 // //array4 - [2, 4, 6]
 // console.log(array4)
@@ -99,24 +99,40 @@ let num = new Number(5);
 //console.log(`${num} `);
 array1 = [1, 2, 3];
 let [, b, c] = array1;
-console.log( b, c);
+console.log(b, c);
 b = 100;
 console.log(array1);
 let d = 4;
 let g = 5;
 //swap
-[d,g] = [g, d];
+[d, g] = [g, d];
 //destructuring object
 function SomeObject() {
     this.h = 10;
-    this.f = function(){console.log("hello")}
+    this.f = function () { console.log("hello") }
 }
 // const obj = {h:10, f: function(){console.log("hello")}}
 const obj = new SomeObject();
-let {f, h} = obj;
+let { f, h } = obj;
 f();
 obj.f();
+Number.prototype.toString = function (str = '0123456789') {
+    // console.log('test');
 
+    return toBinary(this, str);
+}
+function toBinary(num, str) {
+    const res = [];
+
+    do {
+        let rem = num % str.length;
+        num = (num - rem) / str.length;
+        res.unshift(str[rem]);
+    } while (num != 0);
+    return res.join('');
+};
+const num1 = new Number(0xff);
+console.log(`${num1.toString('abc')}`)
 
 
 
